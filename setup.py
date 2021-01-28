074a43234f0be351b7c00371803a2c65e87b4f2b
YounHoyoul <yhy0215@gmail.com>
2021-01-28 11:05:36 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Text-GCN",
+    version="0.1.0",
+    author="Hoyoul Youn",
+    author_email="yhy0215@gmail.com",
+    url="https://github.com/YounHoyoul/Text-GCN.git",
+    license="Apache",
+    packages=find_packages()
+)
